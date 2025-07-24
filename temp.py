c2391311b74d13bdb461db386539d01483946746
Abhiraaj Verma <abhiraajverma@Abhiraajs-MacBook-Air.local>
2025-07-24 17:45:29 +0530

    2. clean input

--- temp.py
+++ temp.py
@@ -12,12 +12,14 @@ def rangeResolver(part):
 
 def expander(s):
     result = []
+    s = s.strip()
     parts = s.split(',')
     
     for part in parts:
-        result.extend(rangeResolver(part))
+        if(part != ''):
+            result.extend(rangeResolver(part))
     return result
 
-input_str = "1-3,5,7-9"
+input_str = "1-3,5,7-9,,  "
 output = expander(input_str)
 print(output)
