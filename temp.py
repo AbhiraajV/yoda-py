d3c8fbdc7e6ebe538eb7551614c273f588d83ac8
Abhiraaj Verma <abhiraajverma@Abhiraajs-MacBook-Air.local>
2025-07-24 17:50:50 +0530

    3. custom delimeters

--- temp.py
+++ temp.py
@@ -1,25 +1,27 @@
-def rangeResolver(part):
-    if '-' in part:
-        start, end = part.split('-')
-        start = int(start)
-        end = int(end)
-        return list(range(start, end + 1))
+def rangeResolver(part,customDelimeters):
+    part = part.strip()
+    for delimeter in customDelimeters:
+        if delimeter in part:
+            start, end = part.split(delimeter)
+            start = int(start)
+            end = int(end)
+            return list(range(start, end + 1))
     else:
         return [int(part)]
 
     
     
 
-def expander(s):
+def expander(s,customDelimeters):
     result = []
     s = s.strip()
     parts = s.split(',')
     
     for part in parts:
         if(part != ''):
-            result.extend(rangeResolver(part))
+            result.extend(rangeResolver(part,customDelimeters))
     return result
 
-input_str = "1-3,5,7-9,,  "
-output = expander(input_str)
+input_str = "3-1,5,7~9,,  "
+output = expander(input_str,['-','~'])
 print(output)
